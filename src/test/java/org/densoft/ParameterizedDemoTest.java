587d5283648b3d46ad0c8efa3ce95212473a0ee6
dennis githinji <wakahiad@gmail.com>
2023-09-03 22:46:30 +0300

    Parameterized tests method provider

--- src/test/java/org/densoft/ParameterizedDemoTest.java
+++ src/test/java/org/densoft/ParameterizedDemoTest.java
@@ -2,10 +2,9 @@ package org.densoft;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -55,6 +54,21 @@ class ParameterizedDemoTest {
     @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
     void csvFileInputCSV(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+
+    @DisplayName("Method Provider test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void fromMethodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    private static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 1, 1),
+                Arguments.of("TY", 3, 4),
+                Arguments.of("KE", 3, 3));
     }
 
 
