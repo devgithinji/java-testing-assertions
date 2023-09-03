057c01127d93766d388e65f95437f2bb9469984b
dennis githinji <wakahiad@gmail.com>
2023-09-03 14:14:21 +0300

    nested tests

---
+++ src/test/java/org/densoft/CalculatorTest.java
@@ -0,0 +1,68 @@
+package org.densoft;
+
+import org.junit.jupiter.api.*;
+
+public class CalculatorTest {
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each nested test context");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each nested test context");
+    }
+
+    @Nested
+    class AdditionTests {
+
+        @BeforeEach
+        void beforeEach() {
+            System.out.println("Before each AdditionTest");
+        }
+
+        @AfterEach
+        void afterEach() {
+            System.out.println("After each AdditionTest");
+        }
+
+        @Test
+        void testPositiveNumbers() {
+            System.out.println("Testing addition of positive numbers");
+            // Test addition of positive numbers
+        }
+
+        @Test
+        void testNegativeNumbers() {
+            System.out.println("Testing addition of negative numbers");
+            // Test addition of negative numbers
+        }
+    }
+
+    @Nested
+    class SubtractionTests {
+
+        @BeforeEach
+        void beforeEach() {
+            System.out.println("Before each SubtractionTest");
+        }
+
+        @AfterEach
+        void afterEach() {
+            System.out.println("After each SubtractionTest");
+        }
+
+        @Test
+        void testPositiveNumbers() {
+            System.out.println("Testing subtraction of positive numbers");
+            // Test subtraction of positive numbers
+        }
+
+        @Test
+        void testNegativeNumbers() {
+            System.out.println("Testing subtraction of negative numbers");
+            // Test subtraction of negative numbers
+        }
+    }
+}
