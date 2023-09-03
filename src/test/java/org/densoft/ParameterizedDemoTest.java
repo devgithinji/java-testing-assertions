aea25548950ba0db10fab8b907514f7dd02129b1
dennis githinji <wakahiad@gmail.com>
2023-09-03 22:02:56 +0300

    Parameterized tests, CSVSource

--- src/test/java/org/densoft/ParameterizedDemoTest.java
+++ src/test/java/org/densoft/ParameterizedDemoTest.java
@@ -1,10 +1,13 @@
 package org.densoft;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ParameterizedDemoTest {
     @DisplayName("value source test ")
@@ -21,4 +24,29 @@ class ParameterizedDemoTest {
     void enumTest(OwnerType ownerType) {
         System.out.println(ownerType);
     }
+
+    @DisplayName("CSV source test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "FL, 1, 2",
+            "KE, 2, 2",
+            "UG, 3, 3"
+    })
+    void csvInputCSV(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "apple,         1",
+            "banana,        2",
+            "'lemon, lime', 0xF1",
+            "strawberry,    7000000"
+    })
+    void testWithCsvSource(String fruit, int rank) {
+        assertNotNull(fruit);
+        assertNotEquals(0, rank);
+    }
+
+
 }
