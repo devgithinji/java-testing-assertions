75efeeae8fcf2e1b07f818be195c29dc1218704a
dennis githinji <wakahiad@gmail.com>
2023-09-03 21:13:17 +0300

    parameterized test Display name

--- src/test/java/org/densoft/ParameterizedDemoTest.java
+++ src/test/java/org/densoft/ParameterizedDemoTest.java
@@ -1,11 +1,12 @@
 package org.densoft;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ParameterizedDemoTest {
-
-    @ParameterizedTest
+    @DisplayName("value source test ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @ValueSource(strings = {"spring", "framework", "guru"})
     void testValueSource(String val) {
         System.out.println(val);
